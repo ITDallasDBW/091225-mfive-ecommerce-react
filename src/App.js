e4a3f7a7cc1eeb31a95823f27c73abf8f36e6740
Blake <32065552+ITDallasDBW@users.noreply.github.com>
2025-09-12 09:44:29 -0500

    Everything's broken or something with old git. I don't understand.

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+import { books } from "./data";
+import BookInfo from "./pages/BookInfo";
+//Testing commit save location
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path="/" exact component={Home} />
+        <Route path="/books" exact render={() => <Books books={books} />} />
+        <Route path="/books/:id" render={() => <BookInfo books={books} />} />
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
