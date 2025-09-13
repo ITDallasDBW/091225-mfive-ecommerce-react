147326c7199d5b26f0dc0c2750bd71f8bfff2ca5
Blake <32065552+ITDallasDBW@users.noreply.github.com>
2025-09-13 17:01:17 -0500

    2.16 Cart complete. 2.17 Loading State begin.

--- src/App.js
+++ src/App.js
@@ -13,29 +13,63 @@ function App() {
 
   function addToCart(book) {
     // console.log('add to cart', book)
-    setCart([...cart, {...book, quantity: 1}]);
+    setCart([...cart, { ...book, quantity: 1 }]);
   }
 
   function changeQuantity(book, quantity) {
-    // console.log(book, quantity)
+    console.log(book, quantity);
+    setCart(
+      cart.map((item) =>
+        item.id === book.id
+          ? {
+              ...item,
+              quantity: +quantity,
+            }
+          : item
+      )
+    );
   }
-  
+
+  function removeItem(item) {
+    setCart(cart.filter(book => book.id !== item.id))
+    console.log("remove item", item);
+  }
+
+  function numberOfItems() {
+    let counter = 0;
+    cart.forEach(item => {
+      counter += item.quantity
+    })
+    return counter;
+  }
+
   useEffect(() => {
-    console.log(cart)
+    console.log(cart);
   }, [cart]);
 
   return (
     <Router>
       <div className="App">
-        <Nav />
+        <Nav numberOfItems={numberOfItems()} />
         <Route path="/" exact component={Home} />
         <Route path="/books" exact render={() => <Books books={books} />} />
         <Route
           path="/books/:id"
           render={() => (
-            <BookInfo books={books} addToCart={addToCart} cart={cart} />)}
+            <BookInfo books={books} addToCart={addToCart} cart={cart} />
+          )}
         />
-        <Route path="/cart" render={() => <Cart books={books} cart={cart} changeQuantity={changeQuantity}/>} />
+        <Route
+          path="/cart"
+          render={() => (
+            <Cart
+              books={books}
+              cart={cart}
+              changeQuantity={changeQuantity}
+              removeItem={removeItem}
+            />
+          )}
+        />
         <Footer />
       </div>
     </Router>
